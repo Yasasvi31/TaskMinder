aa55ea058406de1bb246422e488f2e99a643ab1d
Yasasvi31 <lightfury200231@gmail.com>
2024-05-14 17:46:51 +0530

    taskminder updates

--- app/src/main/java/com/example/labexam4/MainActivity.kt
+++ app/src/main/java/com/example/labexam4/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.labexam4.database.TaskDatabase
+import com.example.labexam4.repository.TaskRepository
+import com.example.labexam4.viewmodel.TaskViewModel
+import com.example.labexam4.viewmodel.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var taskViewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setupViewModel()
+
+    }
+
+    private fun setupViewModel(){
+        val taskRepository = TaskRepository(TaskDatabase(this))
+        val viewModelProviderFactory = TaskViewModelFactory(application,taskRepository)
+        taskViewModel = ViewModelProvider(this,viewModelProviderFactory)[TaskViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/example/labexam4/adapter/TaskAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.labexam4.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.labexam4.databinding.TaskLayoutBinding
+import com.example.labexam4.fragments.HomeFragmentDirections
+import com.example.labexam4.model.Task
+
+class TaskAdapter:RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    class TaskViewHolder(val itemBinding: TaskLayoutBinding):RecyclerView.ViewHolder(itemBinding.root)
+
+    private val differCallback = object :DiffUtil.ItemCallback<Task>(){
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id &&
+                    oldItem.taskTitle == newItem.taskTitle &&
+                    oldItem.taskDate == newItem.taskDate &&
+                    oldItem.taskStatus == newItem.taskStatus &&
+                    oldItem.taskPriority == newItem.taskPriority &&
+                    oldItem.taskDesc == newItem.taskDesc
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    val differ = AsyncListDiffer(this,differCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        return TaskViewHolder(
+            TaskLayoutBinding.inflate(LayoutInflater.from(parent.context),parent)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val currentTask = differ.currentList[position]
+
+        holder.itemBinding.taskTitle.text = currentTask.taskTitle
+        holder.itemBinding.taskDate.text = currentTask.taskDate
+        holder.itemBinding.taskStat.text = currentTask.taskStatus
+        holder.itemBinding.taskPri.text = currentTask.taskPriority
+        holder.itemBinding.taskDesc.text = currentTask.taskDesc
+
+        holder.itemView.setOnClickListener {
+          val direction = HomeFragmentDirections.actionHomeFragmentToEditTaskFragment(currentTask)
+          it.findNavController().navigate(direction)
+        }
+
+
+
+    }
+}
