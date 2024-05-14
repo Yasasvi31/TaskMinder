324dd56eba39abc6da0cfc56df17a6d26ebd200f
Yasasvi31 <lightfury200231@gmail.com>
2024-05-14 20:01:32 +0530

    taskminder updates

--- app/src/main/java/com/example/labexam4/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/labexam4/fragments/HomeFragment.kt
@@ -8,7 +8,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
